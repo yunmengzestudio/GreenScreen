55886fabaef6c0ca549fd36e9d34282c99f85e6a
nuso <nelohjb@hotmail.com>
2019-11-17 18:38:40 +0800

    管理视频的选项卡 UI
    - VideoResourceManager: 选项卡 UI，切换Panel，提供删除/添加视频的路径.
    - OpenFileByWin32(未删) -> VideoResourceAPI

---
+++ Assets/Scripts/UI/VideoResourceManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Video;
+
+public enum VideoType
+{
+    Background = 0,
+    Effect = 1,
+    Product = 2
+}
+
+public class VideoResourceManager : MonoBehaviour
+{
+    public VideoManager video;
+    public List<SelectPanel> Panels;
+
+    private int curPanelIndex = 0;
+    private Dictionary<string, VideoType> Prefix2Type = new Dictionary<string, VideoType>() {
+        {"BG", VideoType.Background}, {"EF", VideoType.Effect}, {"PRO", VideoType.Product}
+    };
+
+
+    private void Start() {
+        if (Panels.Count > 0) {
+            Panels[0].gameObject.SetActive(true);
+        }
+        for(int i = 1; i < Panels.Count; i++) {
+            Panels[i].gameObject.SetActive(false);
+        }
+    }
+
+    // 按钮事件 切换 Panel
+    public void SwitchPanel(int index) {
+        if (index == curPanelIndex) return;
+        Panels[curPanelIndex].gameObject.SetActive(false);
+        Panels[index].gameObject.SetActive(true);
+        curPanelIndex = index;
+    }
+
+    // 按钮事件 添加视频
+    public void AddVideo() {
+        string prefix = Prefix2Type.FirstOrDefault(q => q.Value == ((VideoType)curPanelIndex)).Key;
+        VideoResourceAPI.OpenFile(((VideoType)curPanelIndex).ToString("g"), prefix);
+    }
+
+    // 按钮事件 删除视频
+    public void DeleteVideo() {
+        string name = video.GetComponent<VideoPlayer>().clip.name;
+        string prefix = name.Split('_')[0];
+        string path = Prefix2Type[prefix].ToString("g");
+        VideoResourceAPI.Delete(path, name);
+
+        video.Stop();
+        Panels[(int)Prefix2Type[prefix]].UpdateAll();
+    }
+
+}
---
+++ Assets/Scripts/VideoResourceAPI.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEditor;
+using UnityEngine.Video;
+
+public class VideoResourceAPI : MonoBehaviour
+{
+    // outPath: Resource 下的路径
+    public static void OpenFile(string outDir,string prefix)
+    {
+        FileOpenDialog dialog = new FileOpenDialog();
+
+        dialog.structSize = Marshal.SizeOf(dialog);
+
+        dialog.filter = ".mp4\0*.mp4";
+
+        dialog.file = new string(new char[256]);
+
+        dialog.maxFile = dialog.file.Length;
+
+        dialog.fileTitle = new string(new char[64]);
+
+        dialog.maxFileTitle = dialog.fileTitle.Length;
+
+        dialog.initialDir = UnityEngine.Application.dataPath;  //默认路径
+
+        dialog.title = "Open File Dialog";
+
+        dialog.defExt = "mp4";//显示文件的类型
+        //注意一下项目不一定要全选 但是0x00000008项不要缺少
+        dialog.flags = 0x00080000 | 0x00001000 | 0x00000800 | 0x00000200 | 0x00000008;  //OFN_EXPLORER|OFN_FILEMUSTEXIST|OFN_PATHMUSTEXIST| OFN_ALLOWMULTISELECT|OFN_NOCHANGEDIR
+
+        if (DialogShow.GetOpenFileName(dialog))
+        {
+            string destPath = Application.dataPath + "/Resources/" + outDir + "/Videos/" + prefix + "_" + dialog.fileTitle;
+            Debug.Log(destPath);
+            File.Copy(dialog.file, destPath, true);
+        }
+
+        /// 生成缩略图
+        /// ...
+    }
+
+
+    // path: Resource 下的目录（包含 Videos/ 和 Thumbnails/）
+    public static void Delete(string path, string name) {
+        Debug.Log(Application.dataPath);
+        string destVideoPath = Application.dataPath + "Resource/" + path + "/Videos/" + name + ".mp4";
+        string destVideoPath1 = Application.dataPath + "/Resources/" + path + "/Videos/" + name + ".mp4.meta";
+        string destPngPath = Application.dataPath + "/Resources/" + path + "/Thumbnails/" + name + ".png";
+        string destPngPath1 = Application.dataPath + "/Resources/" + path + "/Thumbnails/" + name + ".png.meta";
+
+        File.Delete(destVideoPath);
+        File.Delete(destVideoPath1);
+        File.Delete(destPngPath);
+        File.Delete(destPngPath1);
+    }
+
+}
+    
+
+   
+
